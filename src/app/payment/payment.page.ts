3e52c664aec5be83978ae17b5b756c3b069627b3
SoftelTechnology <info@softeltech.in>
2023-04-10 18:44:08 +0530

    10042023

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController, NavParams, Platform } from '@ionic/angular';
+import { GlobalVars } from 'src/service/globalvars';
+import { LoaderView } from 'src/service/loaderview';
+import { ConnectServer } from 'src/service/connectserver';
+import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -11,10 +15,89 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class PaymentPage implements OnInit {
-
-  constructor() { }
+  paytm : boolean = false;
+  PaymentLink : any;
+  AllowPayment : any;
+  EnablePaytm : any;
+  Enable_NEFT : any;
+	/*options : InAppBrowserOptions = {
+      location : 'yes',//Or 'no'
+      hidden : 'no', //Or  'yes'
+      clearcache : 'yes',
+      clearsessioncache : 'yes',
+      zoom : 'yes',//Android only ,shows browser zoom controls
+      hardwareback : 'yes',
+      mediaPlaybackRequiresUserAction : 'no',
+      shouldPauseOnSuspend : 'no', //Android only
+      closebuttoncaption : 'Close', //iOS only
+      disallowoverscroll : 'no', //iOS only
+      toolbar : 'yes', //iOS only
+      enableViewportScale : 'no', //iOS only
+      allowInlineMediaPlayback : 'no',//iOS only
+      presentationstyle : 'pagesheet',//iOS only
+      fullscreen : 'yes',//Windows only
+      );*/
+  constructor(private navCtrl: NavController,
+    private globalVars: GlobalVars,
+    private connectServer: ConnectServer,
+    private platform: Platform,
+    private loaderView: LoaderView,
+    private params: NavParams) {
+    this.PaymentLink="";
+    this.AllowPayment ="";
+    this.EnablePaytm = "0";
+    this.Enable_NEFT = "0";
+   }
 
   ngOnInit() {
-  }
+    this.loaderView.showLoader('Loading ...');
+     this.connectServer.getData("Society", null).then(
+        resolve => { 
+                      this.loaderView.dismissLoader();
+                      if(resolve['success'] == 1)
+                      {
 
+                      	var temp = resolve['response']['PaymentGateway']['0'];
+                        this.Enable_NEFT = temp['Record_NEFT'];
+                      	this.AllowPayment= temp['PaymentGateway'];
+
+                        //alert(this.AllowPayment);
+                        if(this.AllowPayment=='1' && temp['Paytm_Link'] !='')
+                        {
+                      		this.paytm = true;
+                          this.PaymentLink=temp['Paytm_Link'];
+                          this.EnablePaytm = "1";
+                      	}
+                        else
+                        {
+                          this.EnablePaytm = "0";
+                         
+                        }
+                        //alert(this.EnablePaytm);
+                      	console.log("paytm = "+this.paytm);
+                      }
+                    }
+     );
+  }
+  public openWithSystemBrowser(){
+ 
+    if(this.paytm == false || this.EnablePaytm =='0')
+    {
+ 
+    alert('Please contact Management to enable payment gateway!');
+      
+    }
+    else
+    {
+      var httpUrl = this.PaymentLink;
+      let target = "_system";
+      //var ref = cordova.InAppBrowser.open(url, target, options);
+     // this.theInAppBrowser.create(httpUrl,target,this.options);
+    }
+  
+ }
+ neftPayment() {
+   alert("Comming Soon!");
+    //this.navCtrl.push(RecordneftPage);
+  }
 }
